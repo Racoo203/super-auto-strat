fd807fc2758a9995d3e217a57ed0d24325adc7fe
Virtual Self <a01722401@tec.mx>
2025-02-09 17:45:21 -0600

    09/02 - 1

--- src/animals.py
+++ src/animals.py
@@ -8,10 +8,10 @@ class Animal:
     def __str__(self):
         return f"{self.name} (ATK: {self.attack}, HP: {self.health}, TIER: {self.tier})"
 
-class Dog(Animal):
+class Ant(Animal):
     def __init__(self):
         super().__init__(
-            name="Dog",
+            name="Ant",
             tier=1,
             attack=2,
             health=5
