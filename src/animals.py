c18d68bd45f779ebb7c5d68612110bea9d4a94ed
Virtual Self <a01722401@tec.mx>
2025-05-16 22:28:23 -0600

    scraping animal data

--- src/animals.py
+++ src/animals.py
@@ -7,12 +7,3 @@ class Animal:
 
     def __str__(self):
         return f"{self.name} (ATK: {self.attack}, HP: {self.health}, TIER: {self.tier})"
-
-class Ant(Animal):
-    def __init__(self):
-        super().__init__(
-            name="Ant",
-            tier=1,
-            attack=2,
-            health=5
-        )
